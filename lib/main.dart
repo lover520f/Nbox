db26bad1fa7ad4293079c4bca5a044b0ad9266cf
Nbox <nbox@example.com>
2026-05-09 10:27:48 +0000

    test: 使用最小化代码测试构建

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,20 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:window_manager/window_manager.dart';
-import 'package:nbox/app.dart';
-import 'package:nbox/core/services/storage_service.dart';
-import 'package:nbox/core/services/config_service.dart';
-import 'package:nbox/core/services/spider_service.dart';
-import 'package:nbox/core/services/player_service.dart';
-import 'package:nbox/core/services/proxy_service.dart';
-import 'package:nbox/core/models/video_source.dart';
 
-const String appVersion = '1.0.0';
+void main() {
+  runApp(const MyApp());
+}
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  
-  await Hive.initFlutter();
-  Hive.registerAdapter(VideoSourceAdapter());
-  Hive.registerAdapter(CategoryAdapter());
-  Hive.registerAdapter(FilterAdapter());
-  
-  await StorageService.init();
-  
-  if (Platform.isWindows || Platform.isMacOS || Platform.isLinux) {
-    await windowManager.ensureInitialized();
-    WindowManager.instance.setMinimumSize(const Size(1280, 720));
-    WindowManager.instance.setMaximumSize(const Size(1920, 1080));
-    await windowManager.setTitle('Nbox 牛盒 v$appVersion');
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nbox',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Nbox - 牛盒')),
+        body: const Center(child: Text('Nbox v1.0.0')),
+      ),
+    );
   }
-  
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.landscapeLeft,
-    DeviceOrientation.landscapeRight,
-    DeviceOrientation.portraitUp,
-  ]);
-  
-  if (Platform.isAndroid || Platform.isIOS) {
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
-  }
-  
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => ConfigService()),
-        ChangeNotifierProvider(create: (_) => SpiderService()),
-        ChangeNotifierProvider(create: (_) => PlayerService()),
-        ChangeNotifierProvider(create: (_) => ProxyService()),
-      ],
-      child: const NboxApp(),
-    ),
-  );
 }

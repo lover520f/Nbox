29d2745984b1f4efd652926dbcd94c19132f6f89
Nbox <nbox@example.com>
2026-05-09 07:23:49 +0000

    Fix Android v1 embedding error - use FlutterActivity and clean build

--- android/app/src/main/kotlin/com/nbox/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/nbox/app/MainActivity.kt
@@ -1,10 +1,6 @@
 package com.nbox.app
 
-import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 }
